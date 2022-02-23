a485e1b710fc0da8e2250902b31cc4e38fbbc013
zmvmax <zmv_max@mail.ru>
2022-02-24 00:05:04 +0200

    edit js

--- js/js.js
+++ js/js.js
@@ -18,7 +18,18 @@ for(let elem of li){
     elem.innerHTML += '!!!';
 }*/
 // let newLi = li.map(elem=>elem.innerHTML += '!!!');
-console.log(li);
+// console.log(li);
 let arr = [1,2,3,4,5,6,7,8,9];
 let newArr = arr.map(elem=>elem*2);
-console.log(arr);
+// console.log(arr);
+
+let p =document.querySelector('p');
+let arr2 = [];
+p.textContent = '!' + p.textContent.replace(/\s/g, '!');
+
+/*p.textContent.split(' ').forEach(item=>arr2.push('!'+item));
+// console.log(arr2)
+p.textContent = arr2.join(' ');*/
+console.log(p);
+
+
