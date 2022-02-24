02bcb17dcf01666e0d3abbf49d7e43c0f5a82ca2
zmvmax <zmv_max@mail.ru>
2022-02-24 22:54:54 +0200

    foreach

--- js/js.js
+++ js/js.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let li = document.querySelectorAll('li');
+/*let li = document.querySelectorAll('li');
 let table = document.querySelector('table');
 
 
@@ -11,7 +11,7 @@ Array.from(table.rows).forEach((tr)=>{
     });
 });
 
-li.forEach(elem=>elem.innerHTML += '!!!');
+li.forEach(elem=>elem.innerHTML += '!!!');*/
 
 /*
 for(let elem of li){
@@ -19,17 +19,61 @@ for(let elem of li){
 }*/
 // let newLi = li.map(elem=>elem.innerHTML += '!!!');
 // console.log(li);
-let arr = [1,2,3,4,5,6,7,8,9];
-let newArr = arr.map(elem=>elem*2);
+/*let arr = [1,2,3,4,5,6,7,8,9];
+let newArr = arr.map(elem=>elem*2);*/
 // console.log(arr);
-
+/*
 let p =document.querySelector('p');
 let arr2 = [];
 p.textContent = '!' + p.textContent.replace(/\s/g, '!');
 
-/*p.textContent.split(' ').forEach(item=>arr2.push('!'+item));
+p.textContent.split(' ').forEach(item=>arr2.push('!'+item));
 // console.log(arr2)
-p.textContent = arr2.join(' ');*/
-console.log(p);
+p.textContent = arr2.join(' ');
+console.log(p);*/
+
+let table = document.querySelector('table');
+let sum = 0;
+Array.from(table.rows).forEach((tr)=>{
+    Array.from(tr.cells).forEach((td, tdId)=>{
+        tdId<3?sum+=Number(td.innerHTML):'';
+        tdId===3?td.innerHTML=sum:'';
+    })
+    sum=0;
+})
+let count = 1;
+Array.from(table.rows).forEach((tr)=>{
+    Array.from(tr.cells).forEach((td, tdId)=>{
+        tdId<3?count*=Number(td.innerHTML):'';
+        tdId===4?td.innerHTML=count:'';
+    })
+    count = 1;
+})
+
+let product = [
+    'молоко',
+    'сыр',
+    'творог',
+    'творог',
+    'молоко',
+    'сметана',
+    'сметана',
+    'молоко',
+];
+let productUniqueObj = {};
+let productUniqueArr = [];
+product.forEach((elem) => {
+    productUniqueObj[elem] = true;
+    !productUniqueArr.includes(elem)?productUniqueArr.push(elem):'';
+});
+console.log(productUniqueArr);
+console.log(Object.keys(productUniqueObj));
 
 
+/*
+Array.from(table.rows).forEach((tr)=>{
+    Array.from(tr.cells).forEach((td,tdId)=>{
+        tdId===0?td.innerHTML+= '!!!':'';
+    });
+});
+*/
