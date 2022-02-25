2f75a9b582cbc9e38df1e87fd3be1eced42bab29
zmvmax <zmv_max@mail.ru>
2022-02-25 23:20:47 +0200

    первое изменение

--- js/map.js
+++ js/map.js
@@ -1,1 +1,18 @@
 'use strict';
+
+let myUsers = [
+    { name: 'shark', likes: 'ocean' },
+    { name: 'turtle', likes: 'pond' },
+    { name: 'otter', likes: 'fish biscuits' }
+]
+
+let usersByLikes = myUsers.map(item => {
+    let container = {};
+
+    container[item.name] = item.likes;
+    container.age = item.name.length * 10;
+
+    return container;
+})
+
+console.log(usersByLikes);
