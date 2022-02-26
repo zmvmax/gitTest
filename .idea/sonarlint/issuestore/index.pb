c2fcb653d1f08da0525c82d4bb55864639103a5f
zmvmax <zmv_max@mail.ru>
2022-02-26 23:15:31 +0200

    испдополнен mapÐÐ

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,6 @@
+
+=
+css/style.css,1\2\126b0f8c1ec95fb3a2541745bfc21365b438d668
+:
+
+index.html,f\6\f6013a00b362253c64368d6eebc50ea2131754e2
--- js/map.js
+++ js/map.js
@@ -1,18 +1,49 @@
 'use strict';
 
-let myUsers = [
-    { name: 'shark', likes: 'ocean' },
-    { name: 'turtle', likes: 'pond' },
-    { name: 'otter', likes: 'fish biscuits' }
+/* калории */
+
+let animals = [
+    { name: 'медведь', likes: 'мед' },
+    { name: 'волк', likes: 'мясо' },
+    { name: 'белка', likes: 'шишки' }
 ]
 
-let usersByLikes = myUsers.map(item => {
-    let container = {};
+let foodCalories = animals.map(item => {
+    let obj = {};
 
-    container[item.name] = item.likes;
-    container.age = item.name.length * 10;
-
-    return container;
+    obj[item.name] = item.likes;
+    obj.calories = item.name.length * 10;
+    return obj;
 })
 
-console.log(usersByLikes);
+console.log(foodCalories);
+
+/* порядковый номер */
+
+let arrSymbols = ['a', 'b', 'c', 'd'];
+let arrResult = arrSymbols.map((item, id)=>(id+1)+item);
+
+console.log(arrResult);
+
+/* студенты и группы */
+
+let obj = [
+    {name: 'Иванов', group: 'ИСП 18-1'},
+    {name: 'Петров', group: 'ИСП 18-2'},
+    {name: 'Сидоров', group: 'ИСП 18-3'},
+];
+
+let arrStudents = obj.map((item, id)=>`${(id+1)} ${item.name} из ${item.group}`);
+
+console.log(arrStudents);
+
+/* номера паспортов */
+
+let arrStudent = [
+    { passport: '03005988', name: 'Иванов', age: 16},
+    { passport: '03005989', name: 'Петров', age: 17},
+    { passport: '03005990', name: 'Сидоров', age: 18,},
+]
+let passports = arrStudent.map(item => item.passport);
+
+console.log(passports);
